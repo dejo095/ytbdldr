0b69caf5d6a69002457bfc3dee1263e28fbb4680
Dejan Brnic <dejan.brnic@aerdata.com>
2018-11-29 16:49:03 +0100

    some work done

--- app.js
+++ app.js
@@ -1,47 +1,98 @@
 const fse = require('fs-extra');
 const path = require('path');
 const express = require('express');
-const cors = require('cors');
-var YoutubeMp3Downloader = require("youtube-mp3-downloader");
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const YoutubeMp3Downloader = require("youtube-mp3-downloader");
+
 const app = express();
 
-app.use(cors());
+let dData = null;
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handlebars middleware
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+hbs.registerHelper('helper_name', function() {
+
+});
+hbs.registerPartials(__dirname + '/views/partials', () => {
+});
 
 // Need ffmpeg lib to convert to mp3
 // https://ffmpeg.zeranoe.com/builds/
 
+// Global variables
+app.use(function(req, res, next){
+    // res.locals.success_msg = req.flash('success_msg');
+    // res.locals.error_msg = req.flash('error_msg');
+    // res.locals.error = req.flash('error');
+    res.locals.data = dData || null;
+    next();
+});
 
-app.get('/download', (req,res) => {
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
-    var URL = req.query.url;
-    var vidId = URL.substring(URL.lastIndexOf("?v=") + 3);
+app.post('/download', (req, res) => {
+    dData = 0;
+    let urlInput = req.body.urlInput;
 
-    var YD = new YoutubeMp3Downloader({
-        "ffmpegPath": "C:/Users/Dejan/Documents/ffmpeg-20181126-90ac0e5-win64-static/bin/ffmpeg.exe",
-        "outputPath": path.join(__dirname, "mp3folder"),
-        "youtubeVideoQuality": "highest",
-        "queueParallelism": 2,
-        "progressTimeout": 4000
-    });
+    if (urlInput.indexOf("?v=") == -1) {
+        // send error
+        return res.json('url not ok');
+    } else {
+        var vidId = urlInput.substring(urlInput.lastIndexOf("?v=") + 3);
 
-    fse.ensureDir(YD.outputPath, err => {
-        if (err) return err;
-        YD.download(vidId);
-    });
+        var YD = new YoutubeMp3Downloader({
+            "ffmpegPath": "C:/Users/Dejan/Documents/ffmpeg-20181126-90ac0e5-win64-static/bin/ffmpeg.exe",
+            "outputPath": path.join(__dirname, "mp3folder"),
+            "youtubeVideoQuality": "highest",
+            "queueParallelism": 2,
+            "progressTimeout": 4000
+        });
 
-    YD.on("finished", function(err, data) {
-        console.log("FINISHED", JSON.stringify(data));
-    });
+        // create dir if nonexistant and initiate download
+        fse.ensureDir(YD.outputPath, err => {
+            if (err) return err;
+            YD.download(vidId);
+        });
 
-    YD.on("error", function(error) {
-        console.log("ERROR", error);
-    });
+        YD.on("finished", function(err, data) {
+            res.redirect('/');
+        });
 
-    // YD.on("progress", function(progress) {
-    //     console.log("PROGRESS", JSON.stringify(progress));
-    // });
+        YD.on("error", function(error) {
+            console.log("ERROR", error);
+        });
 
-})
+        YD.on("progress", function(progress) {
+            console.log('progress', progress);
+            // var data = parseInt(progress.progress.percentage);
+            // setInterval(() => {
+            //     while (dData <= data) {
+            //         console.log("PROGRESS", data);
+            //         dData = data;
+            //     }
+            // }, 500);
+
+        });
+
+
+
+
+    }
+
+
+
+
+
+});
 
 app.listen(4000, () => {
     console.log('Server Works !!! At port 4000');
